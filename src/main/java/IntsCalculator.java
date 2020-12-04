736564e362c082696de2559b937e917b86e7cc88
”wwowann” <ivasin75@mail.ru>
2020-12-04 18:59:40 +0000

    add all

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -1,17 +1,32 @@
-public class IntsCalculator implements Ints{
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
 
     @Override
     public int sum(int arg0, int arg1) {
-        return arg0 + arg1;
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM).result();
     }
 
     @Override
     public int mult(int arg0, int arg1) {
-        return arg0 * arg1;
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT).result();
     }
 
     @Override
-    public int pow(int a, int b) {
-        return (int) Math.pow(a, b);
+    public int pow(int arg0, int arg1) {
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.POW).result();
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,9 @@ public class Main {
         System.out.println("сумма = " + intsCalc.sum(2, 2));
         System.out.println("произведение = " + intsCalc.mult(2, 2));
         System.out.println("степень числа = " + intsCalc.pow(2, 2));
-        System.out.println("сумма = " + intsCalc.sum(10, 22));
-        System.out.println("произведение = " + intsCalc.mult(10, 22));
-        System.out.println("степень числа = " + intsCalc.pow(2, 10));
+        System.out.println("сумма = " + intsCalc.sum(10, 5));
+        System.out.println("произведение = " + intsCalc.mult(10, 5));
+        System.out.println("степень числа = " + intsCalc.pow(10, 5));
 
     }
 }
